ae1d2aa6c7865bd60fa89e9b163cea00384d1abb
Anshu-maury <anshumaurya88979@gmail.com>
2024-03-18 16:34:22 +0530

    save changes

--- app.js
+++ app.js
@@ -1,15 +1,11 @@
-// const heading = React.createElement("h1", {id: "heading"}, "hello world from react")
-//         const root = ReactDOM.createRoot(document.getElementById("root"));
-//         root.render(heading);
-
+import React from "react";
+import ReactDOM from "react-dom/client";
 const parent = React.createElement("div",{id:"parent"},
 React.createElement("div",{id:"child"},[
 React.createElement("h1",{},"Im h1 tag"),
 React.createElement("h2",{},"Im h1 tag")
-
 ])
 );
-// console.log(parent);
+console.log(parent);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
-console.log(root)        
